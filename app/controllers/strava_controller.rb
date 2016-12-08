364fb1ff3a0b28c5fdc07e004f104687f7386164
VS033317 <venkatesh.sridharan@cerner.com>
2016-12-08 11:07:25 -0600

    Added basic strava controller

---
+++ app/controllers/strava_controller.rb
@@ -0,0 +1,12 @@
+class StravaController < ApplicationController
+
+  def index
+    #Calls the oauth endpoint http://app.com/exchange?state=mystate&code=<code from the login screen> to get basic info
+  end
+
+  def show
+    #Calls the athlete information to get the runs for that year
+
+  end
+
+end
