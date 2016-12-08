b51b774d1d9f0e32e097850503199b23c16157a4
ik019542 <ian.kottman@cerner.com>
2016-12-08 16:14:19 -0600

    Try out callback getting token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require 'oauth2'
+require 'httparty'
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
@@ -9,13 +10,16 @@ class ApplicationController < ActionController::Base
     client = OAuth2::Client.new('15075', 'e251bd49ab45ec2ba2fcd1e269453910afcbd200', :site => 'https://www.strava.com/oauth/authorize')
     redirect_to client.auth_code.authorize_url(:redirect_uri => 'http://quantify.ddns.net/callback')
 
-    # token = client.auth_code.get_token('authorization_code_value', :redirect_uri => 'http://localhost:8080/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
     RunningClubMailer.run_log_email.deliver
-    puts 'got ourselves a token'
   end
 
   def callback
     logger.debug("callback called")
-    logger.debug(params.inspect)
+    # the auth token is in the 'code' parameter
+    code = params[:code]
+    client = OAuth2::Client.new('15075', 'e251bd49ab45ec2ba2fcd1e269453910afcbd200', :site => 'https://www.strava.com/oauth/authorize')
+    token = client.auth_code.get_token('code_value', :redirect_uri => 'http://quantify.ddns.net/callback')
+    logger.debug("got a token")
+    logger.debug(token)
   end
 end
