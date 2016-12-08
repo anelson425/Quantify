015c66139c095083b18576bae3a9342aa4e2d46b
Anthony Ross <Anthony.Ross@Cerner.com>
2016-12-08 17:26:33 -0600

    another fix

--- app/helpers/mileage_helper.rb
+++ app/helpers/mileage_helper.rb
@@ -5,10 +5,10 @@ def create_table(activities)
 
   activities.each do |activity|
     table_rows << content_tag(:tr) do
-      content_tag :td activity.date
-      content_tag :td activity.distance_mi
-      content_tag :td activity.time_min
-      content_tag :td activity.total_distance_mi
+      content_tag(:td) activity.date
+      content_tag(:td) activity.distance_mi
+      content_tag(:td) activity.time_min
+      content_tag(:td) activity.total_distance_mi
     end
   end
 end
