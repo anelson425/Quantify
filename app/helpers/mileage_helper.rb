50aae46c521b7f30527da101da3e6d267a9c4aef
Anthony Ross <Anthony.Ross@Cerner.com>
2016-12-08 17:33:32 -0600

    added module

--- app/helpers/mileage_helper.rb
+++ app/helpers/mileage_helper.rb
@@ -1,14 +1,17 @@
 require 'action_view'
 
-def create_table(activities)
-  table_rows = []
+module MileageHelper
 
-  activities.each do |activity|
-    table_rows << content_tag(:tr) do
-      content_tag(:td, activity.date)
-      content_tag(:td, activity.distance_mi)
-      content_tag(:td, activity.time_min)
-      content_tag(:td, activity.total_distance_mi)
+  def create_table(activities)
+    table_rows = []
+
+    activities.each do |activity|
+      table_rows << content_tag(:tr) do
+        content_tag(:td, activity.date)
+        content_tag(:td, activity.distance_mi)
+        content_tag(:td, activity.time_min)
+        content_tag(:td, activity.total_distance_mi)
+      end
     end
   end
 end
