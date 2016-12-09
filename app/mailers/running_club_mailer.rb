fa1184ad85f1035ad33285b5dfc8a2f33c84df98
ik019542 <ian.kottman@cerner.com>
2016-12-09 00:08:19 -0600

    SIZE WAS WRONG

--- app/mailers/running_club_mailer.rb
+++ app/mailers/running_club_mailer.rb
@@ -1,16 +1,14 @@
-require 'pry'
 class RunningClubMailer < ApplicationMailer
   default from: 'cerner.quantify@gmail.com'
 
   def run_log_email(size, athlete, activities)
     @athlete = eval(athlete)
-    @shirt_size = size
+    @size = size
     @activities = Array.new
     activities.each do |a|
       b = JSON.load a
       @activities = @activities << b
     end
-    binding.pry
     mail(to: 'ian.kottman@cerner.com', subject: "#{@athlete['firstname']} #{@athlete['lastname']} - Tracking Card Submission")
   end
 end
