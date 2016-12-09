747b19095a0280775b55d6f9fa13aced4eb966ad
ik019542 <ian.kottman@cerner.com>
2016-12-08 18:37:50 -0600

    Abort failed jquery fix attempt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require turbolinks
-//= require materialize-sprockets
 //= require_tree .
