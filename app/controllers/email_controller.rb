4bae15a7b05d9c0d232e3d9c986a9360fb79d5e6
Anthony Ross <Anthony.Ross@Cerner.com>
2016-12-08 21:31:05 -0600

    initial params work

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,7 @@ class EmailController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def send_email
-    puts 'email controller'
-    RunningClubMailer.run_log_email(params)
+    puts params
+    RunningClubMailer.run_log_email(params).deliver
   end
 end
--- app/mailers/running_club_mailer.rb
+++ app/mailers/running_club_mailer.rb
@@ -1,10 +1,9 @@
 class RunningClubMailer < ApplicationMailer
   default from: 'cerner.quantify@gmail.com'
 
-  def run_log_email(params)
-    puts 'here'
+  def run_log_email(size, athlete, activities)
     @athlete = athlete
-    @shirt_size = shirt_size
+    @shirt_size = size
     @activities = activities
     mail(to: athlete.email, subject: "#{athlete.firstname} #{athlete.lastname} - Tracking Card Submission")
   end
