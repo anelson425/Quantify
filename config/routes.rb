335d895a106cccc763d2409086a4c997cbf3eef7
ik019542 <ian.kottman@cerner.com>
2016-12-08 19:51:56 -0600

    Update email to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'application#index'
   get '/mileage' => 'application#mileage'
-  get '/email' => 'running_club_mailer#run_log_email'
+  post '/email' => 'running_club_mailer#run_log_email'
 end
