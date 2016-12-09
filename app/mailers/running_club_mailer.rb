79e2f53272a26c05b43523a1ee641767daf6be70
ik019542 <ian.kottman@cerner.com>
2016-12-08 19:31:08 -0600

    Add debug to email

--- app/mailers/running_club_mailer.rb
+++ app/mailers/running_club_mailer.rb
@@ -2,6 +2,7 @@ class RunningClubMailer < ApplicationMailer
   default from: 'cerner.quantify@gmail.com'
 
   def run_log_email()
+    logger.debug('hit email')
     mail(to: 'venkatesh.sridharan@cerner.com', subject: "CHEESEBALLS!!!!")
   end
 end
