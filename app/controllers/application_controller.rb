4b78f74506a05640b1fcf4225e5faa890b246ac5
ik019542 <ian.kottman@cerner.com>
2016-12-08 20:45:30 -0600

    Make the email send button not look atrocious

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,7 @@ class ApplicationController < ActionController::Base
 
   def index
     redirect_to @oauth_client.auth_code.authorize_url(:redirect_uri => REDIRECT_URL)
-    # @activities = [Activity.new(date: '1234123434', distance_mi: '23435', time_min: '354642', total_distance_mi: '35452')]
-    # RunningClubMailer.run_log_email.deliver
+    #@activities = [Activity.new(date: '1234123434', distance_mi: '23435', time_min: '354642', total_distance_mi: '35452')]
   end
 
   def mileage
