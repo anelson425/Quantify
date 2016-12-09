34f44ede2d44ffee5426b5cdea26e6d1062d8bdd
ik019542 <ian.kottman@cerner.com>
2016-12-08 23:00:09 -0600

    Added logging

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,6 +3,9 @@ class EmailController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def send_email
+    logger.debug('SENDING THE EMMAAAAAAIL')
+    logger.debug(ENV['QUANTIFY_EMAIL_USER_NAME'])
+    logger.debug(ENV['QUANTIFY_EMAIL_PASSWORD'])
     RunningClubMailer.run_log_email(params[:size], params[:athlete], params[:activities]).deliver
   end
 end
