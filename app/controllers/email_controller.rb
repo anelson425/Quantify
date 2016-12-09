d9b780b0d2b57216e652e9be9d2321b36fce9525
ik019542 <ian.kottman@cerner.com>
2016-12-08 23:39:11 -0600

    Possibly maybe fix the issue where we go to the mileage page after we send an email

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,9 +3,7 @@ class EmailController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def send_email
-    logger.debug('SENDING THE EMMAAAAAAIL')
-    logger.debug(ENV['QUANTIFY_EMAIL_USER_NAME'])
-    logger.debug(ENV['QUANTIFY_EMAIL_PASSWORD'])
     RunningClubMailer.run_log_email(params[:size], params[:athlete], params[:activities]).deliver
+    redirect_to root_url and return
   end
 end
